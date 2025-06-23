1c04bf7a64f72e6922abc688996aa00eb2220bce
Percevale <denis_shagun@mail.ru>
2025-06-23 16:51:12 +0300

    first commit

---
+++ src/main/java/com/example/servicesavedfilm/ServiceSavedFilmApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicesavedfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSavedFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSavedFilmApplication.class, args);
+	}
+
+}
